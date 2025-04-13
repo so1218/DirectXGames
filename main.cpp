17cb9123d8bcbf9200fd89ed16053a110f27f3f5
so1218 <K024G0110@g.neec.ac.jp>
2025-04-14 03:45:12 +0900

    更新

--- main.cpp
+++ main.cpp
@@ -5,6 +5,44 @@
 #include <filesystem>
 #include <fstream>
 #include <chrono>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <cassert>
+#include <DbgHelp.h>
+# include <strsafe.h>
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "Dbghelp.lib")
+
+// string->wstring
+std::wstring ConvertString(const std::string& str) {
+    if (str.empty()) {
+        return std::wstring();
+    }
+
+    auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+    if (sizeNeeded == 0) {
+        return std::wstring();
+    }
+    std::wstring result(sizeNeeded, 0);
+    MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+    return result;
+}
+
+// wstring->string
+std::string ConvertString(const std::wstring& str) {
+    if (str.empty()) {
+        return std::string();
+    }
+
+    auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+    if (sizeNeeded == 0) {
+        return std::string();
+    }
+    std::string result(sizeNeeded, 0);
+    WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
+    return result;
+}
 
 // ウィンドウプロシージャ
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
@@ -29,10 +67,54 @@ void Log(const std::string& message, std::ostream& os)
     OutputDebugStringA(message.c_str());
 }
 
+static LONG WINAPI ExportDump(EXCEPTION_POINTERS* exception)
+{
+    // 時刻を取得して、時刻を名前に入れたファイルを作成。Dumpsディレクトリ以下に出力
+    SYSTEMTIME time;
+    GetLocalTime(&time);
+    wchar_t filePath[MAX_PATH] = { 0 };
+    CreateDirectory(L"./Dumps", nullptr);
+    StringCchPrintfW(filePath, MAX_PATH,
+        L"./Dumps/%04-d%02d%02d-%02d%02d.dmp",
+        time.wYear, time.wMonth, time.wDay,
+        time.wHour, time.wMinute);
+    HANDLE dumpFileHandle = CreateFile(
+        filePath,
+        GENERIC_READ | GENERIC_WRITE,
+        FILE_SHARE_WRITE | FILE_SHARE_READ,
+        0,
+        CREATE_ALWAYS,
+        0,
+        0
+    );
+    // processId(このexeのId)とクラッシュ(例外)の発生したthreadIdを取得
+    DWORD processId = GetCurrentProcessId();
+    DWORD threadId = GetCurrentThreadId();
+    // 設定情報を入力
+    MINIDUMP_EXCEPTION_INFORMATION minidumpInfomation{ 0 };
+	minidumpInfomation.ThreadId = threadId;
+	minidumpInfomation.ExceptionPointers = exception;
+	minidumpInfomation.ClientPointers = TRUE;
+    // Dumpを出力。MiniDumpNormalは最低限の情報を出力するフラグ
+	MiniDumpWriteDump(
+		GetCurrentProcess(),
+		processId,
+		dumpFileHandle,
+		MiniDumpNormal,
+		&minidumpInfomation,
+		nullptr,
+		nullptr
+	);
+    //他に関連付けられているSEH例外ハンドラがあれば実行。通常はプロセスを終了する
+    return EXCEPTION_EXECUTE_HANDLER;
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) 
 {
+    // 誰も捕捉しなかった場合に、補足する関数を登録
+    SetUnhandledExceptionFilter(ExportDump);
+
     // ログのディレクトリを用意
     std::filesystem::create_directory("logs");
 
@@ -90,6 +172,94 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     // ウィンドウを表示する
     ShowWindow(hwnd, SW_SHOW);
 
+    // DXGIファクトリーの生成
+    IDXGIFactory7* dxgiFactory = nullptr;
+    HRESULT hr = CreateDXGIFactory(IID_PPV_ARGS(&dxgiFactory));
+    assert(SUCCEEDED(hr));
+    // 使用するアダプタ用の変数。最初にnullptrを入れておく
+    IDXGIAdapter4* useAdapter = nullptr;
+    // いい順にアダプタを頼む
+    for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(i, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&useAdapter)) != DXGI_ERROR_NOT_FOUND; i++)
+    {
+        // アダプタの情報を取得する
+        DXGI_ADAPTER_DESC3 adapterDesc{};
+        hr = useAdapter->GetDesc3(&adapterDesc);
+        assert(SUCCEEDED(hr));
+        // ソフトウェアアダプタでなければ採用
+        if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE))
+        {
+            // 採用したアダプタの情報をログに出力。
+            Log(ConvertString(std::format(L"Use Adapter: {}\n", adapterDesc.Description)), logStream);
+            break;
+        }
+        // ソフトウェアアダプタの場合は見なかったことにする
+        useAdapter = nullptr;
+    }
+
+    ID3D12Device* device = nullptr;
+    // 機能レベルとログ出力用の文字列
+    D3D_FEATURE_LEVEL featureLevels[] =
+    {
+        D3D_FEATURE_LEVEL_12_2,D3D_FEATURE_LEVEL_12_1,D3D_FEATURE_LEVEL_12_0
+    };
+    const char* featureLevelStrings[] =
+    {
+        "12_2","12_1","12_0"
+    };
+    // 高い順に生成できるか試していく
+    for (size_t i = 0; i < _countof(featureLevels); i++)
+    {
+        // 採用したアダプターでデバイスを生成
+        hr = D3D12CreateDevice(
+            useAdapter,
+            featureLevels[i],
+            IID_PPV_ARGS(&device)
+        );
+        // 指定した機能レベルでデバイスが生成できたかを確認
+        if (SUCCEEDED(hr))
+        {
+            // 生成できたのでログ出力を行ってループを抜ける
+            Log(std::format("Feature Level : {}", featureLevelStrings[i]), logStream);
+            break;
+        }
+    }
+    // デバイスの生成がうまくいかなかったので起動できない
+    assert(device != nullptr);
+    Log("Complete create D3D12Device!!!\n", logStream);// 初期化完了のログを出す
+
+    // 適切なアダプタが見つからなかったので起動できない
+    assert(useAdapter != nullptr);
+
+    // コマンドキューを生成する
+    ID3D12CommandQueue* commandQueue = nullptr;
+    D3D12_COMMAND_QUEUE_DESC commandQueueDesc{};
+	hr = device->CreateCommandQueue(
+		&commandQueueDesc,
+		IID_PPV_ARGS(&commandQueue)
+	);
+    // コマンドキューの生成がうまくいかなかったら起動できない
+	assert(SUCCEEDED(hr));
+
+    // コマンドアロケータを生成する
+    ID3D12CommandAllocator* commandAllocator = nullptr;
+    hr = device->CreateCommandAllocator(
+        D3D12_COMMAND_LIST_TYPE_DIRECT,
+        IID_PPV_ARGS(&commandAllocator)
+    );
+    assert(SUCCEEDED(hr));
+
+    // コマンドリストを生成する
+    ID3D12GraphicsCommandList* commandList = nullptr;
+    hr = device->CreateCommandList(
+        0,
+        D3D12_COMMAND_LIST_TYPE_DIRECT,
+        commandAllocator,
+        nullptr, // 初期化時のPSO（まだ不要ならnullptr）
+        IID_PPV_ARGS(&commandList)
+    );
+    assert(SUCCEEDED(hr));
+
+
 	// 出力ウィンドウへの文字出力
 	OutputDebugStringA("Hello,DirectX!\n");
 
@@ -99,14 +269,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     // 整数を文字列にする
     std::string str1{ std::to_string(10) };
 
-    // string->wstring
-    std::wstring ConvertString(const std::string & str);
-    // wstring->string
-    std::string ConvertString(const std::wstring & str);
-
-    
-
     MSG msg{};
+
     // ウィンドウのxボタンが押されるまでのループ
     while (msg.message != WM_QUIT)
     {
