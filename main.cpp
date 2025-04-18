2014cb7f96c1fd8d40e5d41f20e56dfeeb48b7ec
so1218 <K024G0110@g.neec.ac.jp>
2025-04-18 19:28:09 +0900

    03_01スライド16まで

--- main.cpp
+++ main.cpp
@@ -650,6 +650,43 @@ ID3D12Resource* UploadTextureData(ID3D12Resource* texture, const DirectX::Scratc
     return intermediateResource;
 }
 
+// DepthStencilTexture関数
+ID3D12Resource* CreateDepthStencilTextureResource(ID3D12Device* device, int32_t width, int32_t height)
+{
+    // 生成するResourceの生成
+    D3D12_RESOURCE_DESC resourceDesc{};
+    resourceDesc.Width = width;// Textureの幅
+    resourceDesc.Height = height;// Textureの高さ
+    resourceDesc.MipLevels = 1;// mipmapの数
+    resourceDesc.DepthOrArraySize = 1;// 奥行き or 配列Textureの配列数
+    resourceDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;// DepthStencilとして利用可能なフォーマット
+    resourceDesc.SampleDesc.Count = 1;// サンプリングカウント。1固定。
+    resourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;// 2次元
+    resourceDesc.Flags = D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL;// DepthStencilとして使う通知
+        
+    // 利用するHeapの設定
+    D3D12_HEAP_PROPERTIES heapProperties{};
+    heapProperties.Type = D3D12_HEAP_TYPE_DEFAULT;
+
+    // 深度値のクリア設定
+    D3D12_CLEAR_VALUE depthClearValue{};
+    depthClearValue.DepthStencil.Depth = 1.0f;// 1.0f(最大値)でクリア
+    depthClearValue.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;// フォーマット。Resourceと合わせる
+
+    // Resourceの生成
+    ID3D12Resource* resource = nullptr;
+    HRESULT hr = device->CreateCommittedResource(
+        &heapProperties,// Heapの設定
+        D3D12_HEAP_FLAG_NONE,// Heapの特殊な設定。特になし
+        &resourceDesc,// Resourceの設定
+        D3D12_RESOURCE_STATE_DEPTH_WRITE,// 深度値を書き込む状態にしておく
+        &depthClearValue,// Clear最適値,
+        IID_PPV_ARGS(&resource));
+    assert(SUCCEEDED(hr));
+
+    return resource;
+}
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) 
 {
@@ -1062,7 +1099,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     ID3D12Resource* materialResource = CreateBufferResource(device, sizeof(VertexData));
     // Spraite用の頂点リソースを作る
     ID3D12Resource* vertexResourceSprite = CreateBufferResource(device, sizeof(VertexData) * 6);
-    
+    // DepthStencilTextureをウィンドウのサイズで作成
+    ID3D12Resource* depthStencilResource = CreateDepthStencilTextureResource(device, kClientWidth, kClientHeight);
+
     // マテリアルにデータを書き込む
     Vector4* materialData = nullptr;
     // 書き込むためのアドレスを取得
